783f72fadddfff9c3bbaf05248c8d222c89bf0d0
Lentokone <samuelaikio@gmail.com>
2024-06-15 12:41:54 +0300

    chore: Add Discord bot functionality
    The code changes include adding functionality to create a Discord bot. The bot can respond to user messages containing specific commands like "!help" and "!bye". It uses the `discordgo` package to interact with the Discord API. The `go.mod` and `go.sum` files were also added to manage dependencies.
    
    Please note that the commit message provided above is a suggestion. Feel free to modify it according to your project's commit message conventions.

---
+++ Bot/bot.go
@@ -0,0 +1,55 @@
+package bot
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var BotToken string
+
+func checkNilErr(e error) {
+	if e != nil {
+		log.Fatal("Error message")
+	}
+}
+
+func Run() {
+
+	// create a session
+	discord, err := discordgo.New("Bot " + BotToken)
+	checkNilErr(err)
+
+	// add a event handler
+	discord.AddHandler(newMessage)
+
+	// open session
+	discord.Open()
+	defer discord.Close() // close session, after function termination
+
+	// keep bot running untill there is NO os interruption (ctrl + C)
+	// Log a message to indicate bot is running, and log user what is logged in
+	fmt.Println("Logged in as", discord.State.User.Username)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+}
+
+func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
+	fmt.Println(message.Author.ID, message.Author.Username)
+
+	// respond to user message if it contains `!help` or `!bye`
+	switch {
+	case strings.Contains(message.Content, "!help"):
+		discord.ChannelMessageSend(message.ChannelID, "Hello World😃")
+	case strings.Contains(message.Content, "!bye"):
+		discord.ChannelMessageSend(message.ChannelID, "Good Bye👋")
+		// add more cases if required
+	}
+}
